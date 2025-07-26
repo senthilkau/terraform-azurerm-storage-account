fd9f50cc2f77d2ce6f1063a081dc51a0b928fba9
senthilkau <senthil@hotmail.co.nz>
2025-07-26 14:56:03 +1000

    feat(module): initial storage account module (v0.1.0)

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "account_tier"        { type = string  default = "Standard" }
+variable "account_replication_type" { type = string default = "LRS" }
