974dc99cd791901bba24096159885314d21dc739
senthilkau <senthil@hotmail.co.nz>
2025-07-26 15:47:35 +1000

    update module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "this" {
-  name                     = substr(lower(replace(var.resource_group_name, "-", "")) + "sa", 0, 24)
+  name                     = substr(lower(replace(var.resource_group_name, "-", "")) , 0, 22)  + "sa"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
