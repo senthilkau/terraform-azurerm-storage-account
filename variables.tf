56971a2faeb5efbe0e2f678412c3df6fa1d24e0f
senthilkau <senthil@hotmail.co.nz>
2025-07-26 15:04:43 +1000

    fix(module): convert variables to multiline blocks

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "account_tier"        { type = string  default = "Standard" }
-variable "account_replication_type" { type = string default = "LRS" }
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "account_tier" {
+  type    = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  type    = string
+  default = "LRS"
+}
