01ce5bb0daed87bda0d55eeff95300354457c5db
senthilkau <senthil@hotmail.co.nz>
2025-07-26 16:03:23 +1000

    fix: use sanitized prefix for storage account name

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 resource "azurerm_storage_account" "this" {
-  name = format("%ssa", substr(lower(replace(var.resource_group_name, "-", "")), 0, 20))
+  name = format(
+    "%ssa",
+    substr(
+      lower(replace(replace(var.resource_group_name, "[^a-zA-Z0-9]", ""), "-", "")),
+      0,
+      20
+    )
+  )
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
